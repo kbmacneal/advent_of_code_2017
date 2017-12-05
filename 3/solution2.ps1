bac2e2150f461248a986455a4c633793c0dd13ec
bowmorek <kenneth.b.macneal@gmail.com>
2017-12-05 15:14:02 -0600

    new_plan easier to load, solution2 slow but working

--- 3/solution2.ps1
+++ 3/solution2.ps1
@@ -1,6 +1,4 @@
-function new-spiral
-{
-  Param([int]$max_ring_size)
+Import-Module -Name .\new_plan.psm1 -Force
 
 function new-cell 
 {
@@ -9,164 +7,51 @@ function new-cell
   $properties = [ordered]@{
     'X' = $x
     'Y' = $y
-  }
-
-  $cell = New-Object -TypeName psobject -Property $properties
-
-  return $cell
-}
-
-function get-middlecells 
-{
-  param($first_cell, $second_cell, [string]$direction)
-
-  switch ($direction) {
-    'up' 
-    {
-      [int]$x = $first_cell.X
-
-      [int]$min_y = $first_cell.Y
-
-      [int]$max_y = $second_cell.Y
-
-      $results = New-Object -TypeName 'System.Collections.Generic.List[object]'
-
-      [int]$i = $min_y + 1
-
-      for ($i; $i -lt $max_y; $i++) 
-      {
-        $null = $results.Add($(new-cell -x $x -y $i))
-      }
-
-      return $results
-    }
-    'down' 
-    {
-      [int]$x = $first_cell.X
-        
-      [int]$min_y = $first_cell.Y #2
-        
-      [int]$max_y = $second_cell.Y #-2
-        
-      $results = New-Object -TypeName 'System.Collections.Generic.List[object]'
-        
-      [int]$i = $min_y - 1
-        
-      for ($i; $i -gt $max_y; $i--) 
-      {
-        $null = $results.Add($(new-cell -x $x -y $i))
-      }
-
-      return $results
-    }
-    'left' 
-    {
-      [int]$y = $first_cell.Y
-        
-      [int]$min_x = $first_cell.X
-        
-      [int]$max_x = $second_cell.X
-        
-      $results = New-Object -TypeName 'System.Collections.Generic.List[object]'
-        
-      [int]$i = $min_x - 1
-        
-      for ($i; $i -gt $max_x; $i--) 
-      {
-        $null = $results.Add($(new-cell -x $i -y $y))
-      }
-
-      return $results
-    }
-    'right' 
-    {
-      [int]$y = $first_cell.Y
-        
-      [int]$min_x = $first_cell.X
-        
-      [int]$max_x = $second_cell.X
-        
-      $results = New-Object -TypeName 'System.Collections.Generic.List[object]'
-        
-      [int]$i = $min_x + 1
-        
-      for ($i; $i -lt $max_x; $i++) 
-      {
-        $null = $results.Add($(new-cell -x $i -y $y))
-      }
-
-      return $results
-    }
-    Default 
-    {
-
-    }
+    'Ring' = $null
   }
 }
 
-$grid = New-Object -TypeName 'System.Collections.Generic.List[object]'
-
-[int[]]$rings = 2..$max_ring_size
-
-[int[]]$first_xs = @(0, 1, 1, 0, -1, -1, -1, 0, 1)
-
-[int[]]$first_ys = @(0, 0, 1, 1, 1, 0, -1, -1, -1)
-
-for ($i = 0; $i -lt $first_xs.Count; $i++) 
+function get-cell ($grid, $x, $y) {
+  
+  if(($grid | Where-Object -Property X -eq $x | Where-Object -Property y -eq $y) -eq $null)
+  {
+    $obj = New-Object -TypeName psobject -Property @{'Value' = 0}
+    
+        return $obj
+  }
+else
 {
-  $cell = new-cell -x $first_xs[$i] -y $first_ys[$i]
-  $null = $grid.Add($cell)
+  return $($grid | Where-Object -Property X -eq $x | Where-Object -Property y -eq $y)
 }
 
-foreach($ring in $rings)
+}
+
+$grid = new-spiral -max_ring_size 100
+
+foreach($cell in $grid)
 {
-  $top_left = new-cell -x $(-1*$ring) -y $ring
-  $top_right = new-cell -x $ring -y $ring
-  $bottom_left = new-cell -x $($ring * -1) -y $($ring * -1)
-  $bottom_right = new-cell -x $ring -y $($ring * -1)
-  $curr_ring_first = new-cell -x $ring -y $(($ring * -1)+1)
-  $last_ring_max = new-cell -x $ring -y $($ring + 1)
-
-  $null = $grid.Add($curr_ring_first)
-
-  $points = New-Object 'System.Collections.Generic.List[object]'
-
-  $points = get-middlecells -first_cell $curr_ring_first -second_cell $top_right -direction up
-
-  foreach($point in $points){$null = $grid.Add($point)}
-
-  $null = $grid.Add($top_right)
-
-  $points = New-Object 'System.Collections.Generic.List[object]'
-    
-  $points = get-middlecells -first_cell $top_right -second_cell $top_left -direction left
-
-  foreach($point in $points){$null = $grid.Add($point)}
-
-  $null = $grid.Add($top_left)
-
-  $points = New-Object 'System.Collections.Generic.List[object]'
-    
-  $points = get-middlecells -first_cell $top_left -second_cell $bottom_left -direction down
-
-  foreach($point in $points){$null = $grid.Add($point)}
-
-  $null = $grid.Add($bottom_left)
-
-  $points = New-Object 'System.Collections.Generic.List[object]'
-    
-  $points = get-middlecells -first_cell $bottom_left -second_cell $bottom_right -direction right
-
-  foreach($point in $points){$null = $grid.Add($point)}
-
-  $null = $grid.Add($bottom_right)
-
-  write-verbose "finished ring $ring"
+  $cell.Value = 0
 }
 
-[GC]::Collect()
+$grid[0].Value = 1
 
- return $grid
-}
+for ($i = 1; $i -lt $grid.Count; $i++) 
+{
+  $cell = $grid[$i]  
 
+  $up_cell = get-cell -grid $grid -x $($cell.x) -y $($cell.Y + 1)
+  $down_cell= get-cell -grid $grid -x $($cell.x) -y $($cell.Y - 1)
+  $right_cell= get-cell -grid $grid -x $($cell.x + 1) -y $($cell.Y)
+  $left_cell= get-cell -grid $grid -x $($cell.x - 1) -y $($cell.Y)
 
+  #diagonals
+
+  $diag_up_left_cell = get-cell -grid $grid -x $($cell.x-1) -y $($cell.Y + 1)
+  $diag_up_right= get-cell -grid $grid -x $($cell.x+1) -y $($cell.Y + 1)
+  $diag_down_left= get-cell -grid $grid -x $($cell.x - 1) -y $($cell.Y-1)
+  $diag_down_right= get-cell -grid $grid -x $($cell.x + 1) -y $($cell.Y-1)
+
+  $cell.Value = $($up_cell.Value) + $($down_cell.Value) + $($left_cell.Value) + $($right_cell.Value) + $($diag_up_left_cell.Value) + $($diag_up_right.Value) + $($diag_down_left.Value) + $($diag_down_right.Value)
+} 
+
+$grid | where-object -Property Value -gt 325489 | select -first 1
