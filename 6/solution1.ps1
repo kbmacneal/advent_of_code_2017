45eb2d1b61a9512b3eb151725044191d018a4bb6
bowmorek <kenneth.b.macneal@gmail.com>
2017-12-06 14:00:53 -0600

    prettified

--- 6/solution1.ps1
+++ 6/solution1.ps1
@@ -1,39 +1,44 @@
-function loop-control ($i, $count) {
-    if ($i -ge $($count - 1))
-    {$i = 0}
+function loop-control ($i, $count) 
+{
+  if ($i -ge $($count - 1))
+  {
+    $i = 0
+  }
     
-    else {$i++}
+  else 
+  {
+    $i++
+  }
 
-    return $i
+  return $i
 }
 
 
-$inputs = get-content .\input.txt
+$inputs = Get-Content -Path .\input.txt
 
-$clean = $inputs -split "<TAB>"
+$clean = $inputs -split '<TAB>'
 
 $index = 0
 
-$mem_banks = new-object System.Collections.ArrayList
+$mem_banks = New-Object -TypeName System.Collections.ArrayList
 
-foreach ($item in $clean) {
-    $properties = [ordered]@{
-        'Blocks' = [Convert]::ToInt32($item)
-        'Index'  = $index
-    }
+foreach ($item in $clean) 
+{
+  $properties = [ordered]@{
+    'Blocks' = [Convert]::ToInt32($item)
+    'Index' = $index
+  }
 
-    $obj = New-Object -TypeName psobject -Property $properties
+  $obj = New-Object -TypeName psobject -Property $properties
 
-    $null = $mem_banks.Add($obj)
-    $index++
+  $null = $mem_banks.Add($obj)
+  $index++
 }
 
+$known_states = New-Object -TypeName System.Collections.ArrayList
 
 
-$known_states = New-Object System.Collections.ArrayList
-
-
-$copier = $($mem_banks.Blocks) -join "<TAB>"
+$copier = $($mem_banks.Blocks) -join '<TAB>'
 
 $null = $known_states.add($copier)
 
@@ -43,79 +48,94 @@ $counter = 0
 
 $is_present = $false
 
-do {
+do 
+{
+  #$start = $($mem_banks | sort -Property Blocks -Descending | select -First 1).Index
 
-    #$start = $($mem_banks | sort -Property Blocks -Descending | select -First 1).Index
+  $max = $($mem_banks.Blocks | Measure-Object -Maximum).Maximum
 
-    $max = $($mem_banks.Blocks | measure -Maximum).Maximum
-
-    $blocks = @()
+  $blocks = @()
     
     
-    $mem_banks | where -Property Blocks -eq $max | % {$blocks += $_}
+  $mem_banks |
+  Where-Object -Property Blocks -EQ -Value $max |
+  ForEach-Object -Process {
+    $blocks += $_
+  }
 
-    if ($blocks.Count -eq 1) {
-        $start = $($mem_banks | sort -Property Blocks -Descending | select -First 1).Index
-    }
-    else {
-        $start = $($blocks | sort -Property Index | select -First 1).Index
-    }
+  if ($blocks.Count -eq 1) 
+  {
+    $start = $($mem_banks |
+      Sort-Object -Property Blocks -Descending |
+    Select-Object -First 1).Index
+  }
+  else 
+  {
+    $start = $($blocks |
+      Sort-Object -Property Index |
+    Select-Object -First 1).Index
+  }
 
     
-    $redistribute = $mem_banks[$start].Blocks
+  $redistribute = $mem_banks[$start].Blocks
     
-    $mem_banks[$start].Blocks = 0
+  $mem_banks[$start].Blocks = 0
     
-    if($start -eq $($mem_banks.Count - 1))
+  if($start -eq $($mem_banks.Count - 1))
+  {
+    $i = 0
+  }
+  else 
+  {
+    $i = $($start + 1)
+  }
+  for ($i; $i -lt $mem_banks.Count; $i = loop-control -i $i -count $($mem_banks.Count - 1) ) 
+  {
+    if ($redistribute -gt 0) 
     {
-        $i = 0
+      $mem_banks[$i].Blocks = $mem_banks[$i].Blocks + 1
     }
-    else {
-        $i = $($start + 1)
-    }
-    for ($i; $i -lt $mem_banks.Count; $i = loop-control -i $i -count $($mem_banks.Count - 1) ) {
-
-        if ($redistribute -gt 0) {
-            $mem_banks[$i].Blocks = $mem_banks[$i].Blocks + 1
-        }
         
     
-        $redistribute--
+    $redistribute--
         
-        if ($redistribute -le 0) {
-            break
-        }
-
-
+    if ($redistribute -le 0) 
+    {
+      break
     }
-    foreach ($item in $known_states) {
-        $known = $item
+  }
+  foreach ($item in $known_states) 
+  {
+    $known = $item
 
-        $current = $($mem_banks.Blocks) -join "<TAB>"
+    $current = $($mem_banks.Blocks) -join '<TAB>'
         
 
-        if ($known -eq $current) {
-            $current
-            $null = $known_states.add($current)
-            $is_present = $true
-            break
-        }
-
-        
+    if ($known -eq $current) 
+    {
+      $current
+      $null = $known_states.add($current)
+      $is_present = $true
+      break
     }
-    if ($is_present -eq $false) {
-        $copier = $($mem_banks.Blocks) -join "<TAB>"
+  }
+  if ($is_present -eq $false) 
+  {
+    $copier = $($mem_banks.Blocks) -join '<TAB>'
         
-        $null = $known_states.add($copier)
+    $null = $known_states.add($copier)
         
-        $copier = $null
+    $copier = $null
+  }
     
-    }
-    
-    $counter++
-    
-} until ($is_present -eq $true)
+  $counter++
+}
+until ($is_present -eq $true)
 
-$known_states | %{$_.Tostring().Replace("<TAB>","`t")} | Out-File .\out.txt
+$known_states |
+ForEach-Object -Process {
+  $_.Tostring().Replace('<TAB>',"`t")
+} |
+Out-File -FilePath .\out.txt
 
 
