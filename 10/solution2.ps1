209e7a50b24e1521974bf1fc74d38211645cb4c1
Ken M <kenneth.b.macneal@gmail.com>
2017-12-10 20:38:24 -0600

    some comments

--- 10/solution2.ps1
+++ 10/solution2.ps1
@@ -10,6 +10,7 @@ $skipSize = 0
     foreach ($len in $lengths)
     {
         # Get the indexes of items to reverse, handling wraparound
+        # for a given length greater than the length of the array, will return positions at the beginning
         $indexesToReverse = for ($i=0; $i -lt $len; $i++){ ($curPos + $i) % $list.Count }
 
         # Swap first and last, seconds and penultimate, third and .. etc unto the middle one: 0,-1  1,-2, 2,-3 etc.
@@ -19,13 +20,16 @@ $skipSize = 0
             $list[$indexesToReverse[$i]] = $list[$indexesToReverse[0-($i+1)]]
             $list[$indexesToReverse[0-($i+1)]] = $temp
         }
-
+        # the next position including wraparound, increment the skip value
         $curPos = ($curPos + $len + $skipSize) % $list.count
         $skipSize++
     }
 }
 
+#generates 16 sets of 16
 $sixteens = 0..15|%{ ,((($_*16)..(($_*16)+15)))}
+
+#kid of odd way to do it, two nested loops could have done it
 
 $denseHash = foreach ($set in $sixteens)
 {
