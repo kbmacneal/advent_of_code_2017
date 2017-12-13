d7b663ede500d4f3c8ded4a5d26939313e6d67a4
bowmorek <kenneth.b.macneal@gmail.com>
2017-12-13 09:31:17 -0600

    there we go

---
+++ 13/solution2.ps1
@@ -0,0 +1,40 @@
+
+$input = get-content .\input.txt
+
+$firewalls = @{}
+
+$input | ForEach-Object {
+
+    $split = $_ -split ": "
+
+    [int]$Left = $split[0]
+    
+    [int]$Right = $split[1]
+
+    $firewalls[$Left] = $Right
+}
+
+$time = 0
+
+$caught = $false
+
+while (!$caught) {
+    $caught = $true
+
+    $time
+    for($i=0; $i -lt $($firewalls.Keys | measure -Maximum).Maximum + 1 ; $i++)
+    {
+        if ($firewalls[$i] -ne $null) {
+            if(($i+$time)%(2*$firewalls[$i] - 2) -eq 0)
+            {
+                $caught = $false
+                $time ++
+
+                break
+            }
+        }   
+    
+    }
+}
+
+$time
