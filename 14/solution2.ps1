e20f10cbdc6fb4ced37ea02f758f34f28362dc60
bowmorek <kenneth.b.macneal@gmail.com>
2017-12-14 13:33:12 -0600

    didnt like the old format

--- 14/solution2.ps1
+++ 14/solution2.ps1
@@ -1,132 +1,145 @@
-class cell {
-    [int]$x;
-    [int]$y;
-    [bool]$Occupied;
-    [int]$Link_Num;
+class cell
+{
+	[int]$x;
+	[int]$y;
+	[bool]$Occupied;
+	[int]$Link_Num;
 }
 
-function get-knothash {
-    # Parameter help description
-    Param(
-        [Parameter(ValueFromPipeline = $true)]
-        [string]$input_string
-    )
+function get-knothash
+{
+	# Parameter help description
+	Param(
+		[Parameter(ValueFromPipeline = $true)]
+		[string]$input_string
+	)
 
 
-    $lengths = @([int[]][System.Text.Encoding]::ASCII.GetBytes($input_string)) + @(17, 31, 73, 47, 23)
-    $list = 0..255
+	$lengths = @([int[]][System.Text.Encoding]::ASCII.GetBytes($input_string)) + @(17, 31, 73, 47, 23)
+	$list = 0..255
     
-    $curPos = 0
-    $skipSize = 0
+	$curPos = 0
+	$skipSize = 0
     
     
-    0..63 | ForEach-Object {
+	0..63 | ForEach-Object {
     
-        foreach ($len in $lengths) {
-            # Get the indexes of items to reverse, handling wraparound
-            # for a given length greater than the length of the array, will return positions at the beginning
-            $indexesToReverse = for ($i = 0; $i -lt $len; $i++) { ($curPos + $i) % $list.Count }
+		foreach ($len in $lengths)
+		{
+			# Get the indexes of items to reverse, handling wraparound
+			# for a given length greater than the length of the array, will return positions at the beginning
+			$indexesToReverse = for ($i = 0; $i -lt $len; $i++) { ($curPos + $i) % $list.Count }
     
-            # Swap first and last, seconds and penultimate, third and .. etc unto the middle one: 0,-1  1,-2, 2,-3 etc.
-            for ($i = 0; $i -lt [int]($indexesToReverse.Count / 2); $i++) {
-                $temp = $list[$indexesToReverse[$i]]
-                $list[$indexesToReverse[$i]] = $list[$indexesToReverse[0 - ($i + 1)]]
-                $list[$indexesToReverse[0 - ($i + 1)]] = $temp
-            }
-            # the next position including wraparound, increment the skip value
-            $curPos = ($curPos + $len + $skipSize) % $list.count
-            $skipSize++
-        }
-    }
+			# Swap first and last, seconds and penultimate, third and .. etc unto the middle one: 0,-1  1,-2, 2,-3 etc.
+			for ($i = 0; $i -lt [int]($indexesToReverse.Count / 2); $i++)
+			{
+				$temp = $list[$indexesToReverse[$i]]
+				$list[$indexesToReverse[$i]] = $list[$indexesToReverse[0 - ($i + 1)]]
+				$list[$indexesToReverse[0 - ($i + 1)]] = $temp
+			}
+			# the next position including wraparound, increment the skip value
+			$curPos = ($curPos + $len + $skipSize) % $list.count
+			$skipSize++
+		}
+	}
     
-    #generates 16 sets of 16
-    $sixteens = 0..15| % { , ((($_ * 16)..(($_ * 16) + 15)))}
+	#generates 16 sets of 16
+	$sixteens = 0..15| % { , ((($_ * 16)..(($_ * 16) + 15)))}
     
-    #kid of odd way to do it, two nested loops could have done it
+	#kid of odd way to do it, two nested loops could have done it
     
-    $denseHash = foreach ($set in $sixteens) {
-        $out = $list[$set[0]]
-        foreach ($index in $set[1..15]) { $out = $out -bxor $list[$index] }
-        $out
-    }
+	$denseHash = foreach ($set in $sixteens)
+ {
+		$out = $list[$set[0]]
+		foreach ($index in $set[1..15]) { $out = $out -bxor $list[$index] }
+		$out
+	}
     
-    return -join ($denseHash | foreach { '{0:x2}' -f $_ })
+	return -join ($denseHash | foreach { '{0:x2}' -f $_ })
     
-    #from https://www.reddit.com/r/adventofcode/comments/7irzg5/2017_day_10_solutions/dr12q92/
+	#from https://www.reddit.com/r/adventofcode/comments/7irzg5/2017_day_10_solutions/dr12q92/
 }
 
-function get-connectedcells($cell_list, [cell]$cell_start, [int]$regnum) {
-    $connected = new-object System.Collections.ArrayList
-    $up_cell = $cell_list | Where-Object -Property X -EQ $cell_start.X | Where-Object -Property Y -eq $($cell_start.Y + 1)
-    $down_cell = $cell_list | Where-Object -Property X -EQ $cell_start.X | Where-Object -Property Y -eq $($cell_start.Y + 1)
-    $left_cell = $cell_list | Where-Object -Property X -EQ $($cell_start.X - 1)| Where-Object -Property Y -eq $cell_start.Y
-    $right_cell = $cell_list | Where-Object -Property X -EQ $($cell_start.X + 1) | Where-Object -Property Y -eq $cell_start.Y
+function get-connectedcells($cell_list, [cell]$cell_start, [int]$regnum)
+{
+	$connected = new-object System.Collections.ArrayList
+	$up_cell = $cell_list | Where-Object -Property X -EQ $cell_start.X | Where-Object -Property Y -eq $($cell_start.Y + 1)
+	$down_cell = $cell_list | Where-Object -Property X -EQ $cell_start.X | Where-Object -Property Y -eq $($cell_start.Y + 1)
+	$left_cell = $cell_list | Where-Object -Property X -EQ $($cell_start.X - 1)| Where-Object -Property Y -eq $cell_start.Y
+	$right_cell = $cell_list | Where-Object -Property X -EQ $($cell_start.X + 1) | Where-Object -Property Y -eq $cell_start.Y
 
-    [void]$connected.Add($up_cell)
-    [void]$connected.Add($down_cell)
-    [void]$connected.Add($left_cell)
-    [void]$connected.Add($right_cell)
+	[void]$connected.Add($up_cell)
+	[void]$connected.Add($down_cell)
+	[void]$connected.Add($left_cell)
+	[void]$connected.Add($right_cell)
 
-    foreach ($cell in $connected) {
-        if ($cell -ne $null) {
-            if ($cell.Occupied -eq $true -and $cell.link_num -eq $null) {
-                $cell.Link_Num = $regnum
-                get-connectedcells -cell_list $cell_list -cell_start $cell -regnum $regnum
-            }
-        }
+	foreach ($cell in $connected)
+ {
+		if ($cell -ne $null)
+		{
+			if ($cell.Occupied -eq $true -and $cell.link_num -eq $null)
+			{
+				$cell.Link_Num = $regnum
+				get-connectedcells -cell_list $cell_list -cell_start $cell -regnum $regnum
+			}
+		}
         
         
-    }
+	}
 
 }
 
 
 $hex = @{
-    '0' = '0000'
-    '1' = '0001'
-    '2' = '0010'
-    '3' = '0011'
-    '4' = '0100'
-    '5' = '0101'
-    '6' = '0110'
-    '7' = '0111'
-    '8' = '1000'
-    '9' = '1001'
-    'a' = '1010'
-    'b' = '1011'
-    'c' = '1100'
-    'd' = '1101'
-    'e' = '1110'
-    'f' = '1111'
+	'0' = '0000'
+	'1' = '0001'
+	'2' = '0010'
+	'3' = '0011'
+	'4' = '0100'
+	'5' = '0101'
+	'6' = '0110'
+	'7' = '0111'
+	'8' = '1000'
+	'9' = '1001'
+	'a' = '1010'
+	'b' = '1011'
+	'c' = '1100'
+	'd' = '1101'
+	'e' = '1110'
+	'f' = '1111'
 }
 
-function generate-grid() {
+function generate-grid()
+{
 
-    function new-cell {
-        Param($x, $y)
+	function new-cell
+ {
+		Param($x, $y)
     
-        $properties = @{
-            'X'        = $x
-            'Y'        = $y
-            'Occupied' = $false
-            'Link_Num' = $null
-        }
+		$properties = @{
+			'X'        = $x
+			'Y'        = $y
+			'Occupied' = $false
+			'Link_Num' = $null
+		}
     
-        $cell = New-Object -TypeName cell -Property $properties
+		$cell = New-Object -TypeName cell -Property $properties
     
-        return $cell
-    }
+		return $cell
+	}
 
-    $grid = new-object system.collections.arraylist
+	$grid = new-object system.collections.arraylist
 
-    for ($i = 0; $i -lt 128; $i++) {
-        for ($j = 0; $j -lt 128; $j++) {
-            $cell = new-cell -x $i -y $j
-            $grid.add($cell)
-        }
-    }
+	for ($i = 0; $i -lt 128; $i++)
+ {
+		for ($j = 0; $j -lt 128; $j++)
+		{
+			$cell = new-cell -x $i -y $j
+			$grid.add($cell)
+		}
+	}
 
-    return $grid
+	return $grid
 
 }
 
@@ -137,7 +150,7 @@ $inputs = "jzgqcdpd"
 $grid = generate-grid
 
 $rows = 0..127 | foreach {
-    (get-knothash "$inputs-$_")
+	(get-knothash "$inputs-$_")
 }
 
 $counter = 0
@@ -149,25 +162,30 @@ $binary_Str = -join ($rows| foreach {$_.getenumerator().foreach{$hex["$_"]}})
 $binary_Str.GetEnumerator() | foreach {[void]$binary_Str_array.Add([int]$_)}
 
 
-for ($i = 0; $i -lt 128; $i++) {
-    for ($j = 0; $j -lt 128; $j++) {
+for ($i = 0; $i -lt 128; $i++)
+{
+	for ($j = 0; $j -lt 128; $j++)
+ {
         
-        $($grid | Where-Object -Property X -eq $j | Where-Object -Property Y -eq $i).Occupied = [Convert]::ToBoolean([int]$binary_Str_array[$counter])
-        $counter++
-    }
+		$($grid | Where-Object -Property X -eq $j | Where-Object -Property Y -eq $i).Occupied = [Convert]::ToBoolean([int]$binary_Str_array[$counter])
+		$counter++
+	}
 }
 
 $region_number = 0
 
-for ($i = 0; $i -lt 128; $i++) {
-    for ($j = 0; $j -lt 128; $j++) {
+for ($i = 0; $i -lt 128; $i++)
+{
+	for ($j = 0; $j -lt 128; $j++)
+ {
         
-        $cell = $grid | Where-Object -Property X -eq $j | Where-Object -Property Y -eq $i
-        if ($cell.link_num -eq $null) {
-            $region_number++
-            get-connectedcells -cell_list $grid -cell_start $cell -regnum $region_number
-        }
-    }
+		$cell = $grid | Where-Object -Property X -eq $j | Where-Object -Property Y -eq $i
+		if ($cell.link_num -eq $null)
+		{
+			$region_number++
+			get-connectedcells -cell_list $grid -cell_start $cell -regnum $region_number
+		}
+	}
 }
 
 
