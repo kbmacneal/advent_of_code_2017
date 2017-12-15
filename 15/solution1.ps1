1120fe92d7e2ac5d24716b6324f63de112409712
bowmorek <kenneth.b.macneal@gmail.com>
2017-12-15 10:06:01 -0600

    takes a while but it should work

---
+++ 15/solution1.ps1
@@ -0,0 +1,97 @@
+$factor_a = 16807
+$factor_b = 48271
+
+$gen_a_start = 722
+$gen_b_start = 354
+
+#use modulo
+$shared_divisor = 2147483647
+
+$hex = @{
+	'0' = '0000'
+	'1' = '0001'
+	'2' = '0010'
+	'3' = '0011'
+	'4' = '0100'
+	'5' = '0101'
+	'6' = '0110'
+	'7' = '0111'
+	'8' = '1000'
+	'9' = '1001'
+	'a' = '1010'
+	'b' = '1011'
+	'c' = '1100'
+	'd' = '1101'
+	'e' = '1110'
+	'f' = '1111'
+}
+
+function calc_value([System.Uint64]$value, $multiple, $divisor)
+{
+	[System.Uint64]$val = ($value * $multiple) % $divisor
+
+	return $val
+}
+
+function convert_to_bin([System.Uint64]$value)
+
+{
+	$binary_Str_array = new-object 'System.Collections.Generic.List[int]'
+
+	$null = -join ($value.tostring().getenumerator() | foreach {$hex["$_"]} | foreach {[void]$binary_Str_array.Add([int]$_)})
+
+	#$binary_Str = -join ($rows| foreach {$_.getenumerator().foreach{$hex["$_"]}})
+
+	#$binary_Str.GetEnumerator() | foreach {[void]$binary_Str_array.Add([int]$_)}
+
+	[string]$final_bin_string = $binary_Str_array -join ""
+
+	if ($final_bin_string.ToString().Length -le 16)
+	{
+		return $final_bin_string.ToString()
+	}
+	else
+ {
+		return $final_bin_string.ToString().Substring($($final_bin_string.ToString().Length - 16),16)
+	}
+
+	
+}
+
+$count = 0
+
+$a_val_list = new-object 'System.Collections.Generic.List[System.Uint64]'
+
+$b_val_list = new-object 'System.Collections.Generic.List[System.Uint64]'
+
+[void]$a_val_list.Add($gen_a_start)
+[void]$b_val_list.Add($gen_b_start)
+
+
+for ($i = 0; $i -lt 4000000; $i++)
+{
+	
+	[System.Uint64]$a_val = calc_value -value $($a_val_list[$a_val_list.Count - 1]) -multiple $factor_a -divisor $shared_divisor
+	[void]$a_val_list.Add($a_val)
+	$a_compare = convert_to_bin -value $a_val
+	
+	[System.Uint64]$b_val = calc_value -value $($b_val_list[$b_val_list.Count - 1]) -multiple $factor_b -divisor $shared_divisor
+	[void]$b_val_list.Add($b_val)
+	$b_compare = convert_to_bin -value $b_val
+
+	if ($a_compare -eq $b_compare)
+	{
+		$count++
+	}
+	Write-Progress -Activity "calculating list" -Status $count.tostring() -PercentComplete $(($i/4000000)*100)
+	
+
+	#[Math]::Round([System.Decimal]$($i / 4000000), [System.MidpointRounding]::AwayFromZero)
+
+	#$i
+	
+}
+
+write-progress -completed
+
+$count
