b6a5c0eef281c4eeecc15f0ea38fc3226498b8c5
Ken M <kenneth.b.macneal@gmail.com>
2017-12-18 19:29:39 -0600

    update

--- 18/non_parallel.ps1
+++ 18/non_parallel.ps1
@@ -13,6 +13,7 @@ class Program {
     [int]$program_id;
     [bool]$locked = $false
     [int]$send_count = 0;
+    [int]$position = 0;
     snd($command) {
         $parse_test = 0
         $registers = $this.regs
@@ -85,12 +86,51 @@ class Program {
             }
         }
         else {
-            if ([long]$registers[$($command.register)] -gt 0) {
-                $i = $i + $registers[$($command.register)] - 1
+            if ([long]$registers[$($command.value)] -gt 0) {
+                $i = $i + $registers[$($command.value)] - 1
             }
         }
 
         return $i
+    }
+    execute_next($command_list) {
+        if ($this.position -gt $command_list.count) {
+            $this.locked = $true
+        }
+
+        $command = $command_list[$($this.position)]
+
+        switch ($command.command) {
+            "set" { 
+                $this.set($command)
+            }
+            "add" { 
+                $this.add($command)
+            }
+            "mul" {
+                $this.mul($command)
+            }
+            "mod" { 
+                $this.mod($command)
+            }
+            "snd" {
+                $this.snd($command)
+            }
+            "rcv" {
+                $this.rcv($command)
+            }
+            "jgz" { 
+                $this.position = $this.jgz($command, $this.position)
+            }
+            Default {}
+        }
+
+        if ($this.locked) {
+            return
+        }
+        else {
+            $this.position++
+        }
     }
 
 }
@@ -132,83 +172,28 @@ $program2.locked = $false
 $program1.otherqueue = $program2.queue
 $program2.otherqueue = $program1.queue
 
-$position1 = 0
-$position2 = 0
+$program1.position = 0
+$program2.position = 0
 
-foreach($command in $command_array)
-{
-	if(!($program1.regs[$command.register]) -and $command.register -match "[a-z]")
-	{
-		$program1.regs[$command.register] = 0
-	}
-	if(!($program2.regs[$command.register]) -and $command.register -match "[a-z]")
-	{
-		$program2.regs[$command.register] = 0
-	}
+foreach ($command in $command_array) {
+    if (!($program1.regs[$command.register]) -and $command.register -match "[a-z]") {
+        $program1.regs[$command.register] = 0
+    }
+    if (!($program2.regs[$command.register]) -and $command.register -match "[a-z]") {
+        $program2.regs[$command.register] = 0
+    }
 }
+$count = 0
 
-do {
-    $command1 = $command_array[$position1]
-    $command2 = $command_array[$position2]
+while ($true) {
+    
+    $count++
 
-    switch ($command1.command) {
-        "set" { 
-            $program1.set($command1)
-        }
-        "add" { 
-            $program1.add($command1)
-        }
-        "mul" {
-            $program1.mul($command1)
-        }
-        "mod" { 
-            $program1.mod($command1)
-        }
-        "snd" {
-            $program1.snd($command1)
-        }
-        "rcv" {
-            $program1.rcv($command1)
-        }
-        "jgz" { 
-            $position1 = $program1.jgz($command1, $position1)
-        }
-        Default {}
+    $program1.execute_next($command_array)
+    $program2.execute_next($command_array)
+
+    if ($program1.locked -eq $true -and $program2.locked -eq $true) {
+        return $program1.send_count
     }
-
-    switch ($command2.command) {
-        "set" { 
-            $program2.set($command2)
-        }
-        "add" { 
-            $program2.add($command2)
-        }
-        "mul" {
-            $program2.mul($command2)
-        }
-        "mod" { 
-            $program2.mod($command2)
-        }
-        "snd" {
-            $program2.snd($command2)
-        }
-        "rcv" {
-            $program2.rcv($command2)
-        }
-        "jgz" { 
-            $position2 = $program2.jgz($command2, $position2)
-        }
-        Default {}
-    }
-
-    if ($program1.locked -eq $false) {
-        $position1++
-    }
-    if ($program2.locked -eq $false) {
-        $position2++
-	}
 	
-
-} until (($program1.locked -eq $true -and $program2.locked -eq $true))
-
-return $program1.send_count
+}
