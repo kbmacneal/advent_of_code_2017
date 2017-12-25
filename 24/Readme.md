323dfc74b4093cf56e6a310b79c29ff7da8207cc
Ken M <kenneth.b.macneal@gmail.com>
2017-12-24 19:09:39 -0600

    update

---
+++ 24/Readme.md
@@ -0,0 +1,1 @@
+Another one that powershell is unlikely to do in total. The number of recursions would probably be too high. I'll come back to this one and see if I can deconstruct some of the recursions to make the callstack smaller.
--- 24/day24/Program.cs
+++ 24/day24/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Collections.Immutable;
 
 namespace day24 {
     class Program {
         static void Main () {
-            var lines = (System.IO.File.ReadAllLines("input.txt")).ToList ();
+            var lines = (System.IO.File.ReadAllLines ("input.txt")).ToList ();
 
             IImmutableList<(int, int)> edges = ImmutableList<(int, int)>.Empty;
 
@@ -25,8 +25,8 @@ namespace day24 {
                 return e.Where (x => x.Item1 == cur || x.Item2 == cur).Select (x => Search2 (e.Remove (x), x.Item1 == cur ? x.Item2 : x.Item1, strength + x.Item1 + x.Item2, length + 1)).Concat (Enumerable.Repeat ((strength, length), 1)).OrderByDescending (x => x.Item2).ThenByDescending (x => x.Item1).First ();
             }
 
-            Console.WriteLine(Search(edges));
-            Console.WriteLine((Search2(edges).Item1));
+            Console.WriteLine (Search (edges));
+            Console.WriteLine ((Search2 (edges).Item1));
 
             //Search (edges).Dump ("part 1");
             //Search2 (edges).Item1.Dump ("part 2");
