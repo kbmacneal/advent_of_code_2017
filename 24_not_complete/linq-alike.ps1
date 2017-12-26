dab5841217ba97d3241a63725c850b16985c41dd
bowmorek <kenneth.b.macneal@gmail.com>
2017-12-26 16:24:15 -0600

    that should do the trick

--- 24_not_complete/linq-alike.ps1
+++ 24_not_complete/linq-alike.ps1
@@ -2,14 +2,30 @@ $lines = get-content .\input.txt
 
 $edges = New-Object System.Collections.Arraylist
 
-function do_search ($list, $current = 0, $strength = 0, $strength_list)
+function do_search ($list, [int]$current = 0, [int]$strength = 0, $strength_list)
 {
 
 	$compares = $list | ? {$_.Item1 -eq $current -or $_.Item2 -eq $current}
 
 	
 
-	$compares | % {[void]$list.Remove($_);if ($_.item1 -eq $current) {$x = $_.Item2}else {$x = $_.Item1}; $st = $strength + $_.Item1 + $_.Item2; [void]$strength_list.Add($st);do_search -list $list -current $x -strength ($strength + $_.Item1 + $_.Item2) -strength_list $strength_list} 
+	$compares | % {
+
+		#$sublist = new-object System.Collections.Arraylist
+		$sublist = $list.Clone()
+		
+		[void]$sublist.Remove($_)
+		
+		if ($_.item1 -eq $current) 
+		{$x = $_.Item2}
+		else {$x = $_.Item1}
+		
+		$st = $strength + $_.Item1 + $_.Item2
+		
+		[void]$strength_list.Add($st)
+		
+		do_search -list $sublist -current $x -strength $st -strength_list $strength_list
+	} 
 	
 	
 	
