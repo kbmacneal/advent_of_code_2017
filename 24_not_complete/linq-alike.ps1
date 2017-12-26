8fb15bf992637f95ab0db8e34407a7450e05cb7a
bowmorek <kenneth.b.macneal@gmail.com>
2017-12-26 16:14:19 -0600

    closer

--- 24_not_complete/linq-alike.ps1
+++ 24_not_complete/linq-alike.ps1
@@ -2,23 +2,16 @@ $lines = get-content .\input.txt
 
 $edges = New-Object System.Collections.Arraylist
 
-function do_search ($list, $current = 0, $strength = 0)
+function do_search ($list, $current = 0, $strength = 0, $strength_list)
 {
 
 	$compares = $list | ? {$_.Item1 -eq $current -or $_.Item2 -eq $current}
 
 	
 
-	[Linq.Enumberable]::Concat($($compares | % {[void]$list.Remove($_);if ($_.item1 -eq $current) {$x = $_.Item1}else {$x = $_.Item2};do_search -list $list -current $x -strength ($strength + $_.Item1 + $_.Item2)}),
+	$compares | % {[void]$list.Remove($_);if ($_.item1 -eq $current) {$x = $_.Item2}else {$x = $_.Item1}; $st = $strength + $_.Item1 + $_.Item2; [void]$strength_list.Add($st);do_search -list $list -current $x -strength ($strength + $_.Item1 + $_.Item2) -strength_list $strength_list} 
 	
 	
-	<#if (($strength + $_.Item1 + $_.Item2) -gt $strength)
-	{
-		return [int]$($strength + $_.Item1 + $_.Item2)
-	}
-	else {
-		return [int]$strength
-	}#>
 	
 }
 
@@ -30,6 +23,15 @@ foreach ($line in $lines)
 	[void]$edges.Add($tuple)
 }
 
+$str_list = New-Object System.Collections.Arraylist
+
+$str_list.Add(0)
 
 
-do_search -list $edges -current 0 -strength 0
+do_search -list $edges -current 0 -strength 0 -strength_list $str_list
+
+$str_list | Measure-Object -Maximum
+
+
+#1695
+#1673
