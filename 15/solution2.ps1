9c4be7c3e6c3a7d4b49eaac7db3741b0bba1830f
bowmorek <kenneth.b.macneal@gmail.com>
2017-12-28 10:07:06 -0600

    finally working

--- 15/solution2.ps1
+++ 15/solution2.ps1
@@ -4,63 +4,51 @@ $factor_b = 48271
 $gen_a_start = 722
 $gen_b_start = 354
 
-#use modulo
 $shared_dividend = 2147483647
 
-function calc_value([long]$value, $multiple, $dividend, $check) {
-    [long]$val = (($value * $multiple) % $dividend) % $check
-
-    if ($val -eq 0) {
-        return (($value * $multiple) % $dividend)
-    }
-    else {
-        return 0
-    }
-}
-
-function convert_to_bin([long]$value) {
-    $bitmask = 65535
-
-    return ($value -band $bitmask)	
-}
-
 $count = 0
-
-$a_val_list = new-object 'System.Collections.Generic.List[long]'
-
-$b_val_list = new-object 'System.Collections.Generic.List[long]'
-
-[void]$a_val_list.Add($gen_a_start)
-[void]$b_val_list.Add($gen_b_start)
 
 $a_compare_list = new-object 'System.Collections.Generic.List[int]'
 
 $b_compare_list = new-object 'System.Collections.Generic.List[int]'
 
+$a_val = $gen_a_start
+$b_val = $gen_b_start
 
-for ($i = 0; $i -lt 5000000; $i++) {
-	$last_val = $a_val_list[$($a_val_list.count-1)]
-    [long]$a_val = calc_value -value $last_val -multiple $factor_a -dividend $shared_dividend -check 4
-    [void]$a_val_list.Add($a_val)
-    if ($a_val -gt 0) {
-        $a_compare = convert_to_bin -value $a_val
-        [void]$a_compare_list.add($a_compare)
-    } 
+$bitmask = 65535
+
+
+while (($a_compare_list.Count -lt 5000000) -or ($b_compare_list.Count -lt 5000000))
+{
+	$prev_a = $a_val
+	$prev_b = $b_val
     
-    $last_val = $b_val_list[$($b_val_list.count-1)]
-    [long]$b_val = calc_value -value $last_val -multiple $factor_b -dividend $shared_dividend -check 8
-    [void]$b_val_list.Add($b_val)
-    if ($b_val -gt 0) {
-        $b_compare = convert_to_bin -value $b_val
-        [void]$b_compare_list.add($b_compare)
+    
+    [long]$a_val = ($prev_a * $factor_a) % $shared_dividend
+    $temp = $a_val % 4
+
+	if ($temp -eq 0)
+ {
+		[void]$a_compare_list.Add($a_val)
+    }
+
+    [long]$b_val = ($prev_b * $factor_b) % $shared_dividend
+    $temp = $b_val % 8
+
+	if ($temp -eq 0)
+ {
+		[void]$b_compare_list.Add($b_val)
     }
 }
 
-for ($i = 0; $i -lt $a_compare_list.count; $i++) {
-    if ($a_compare_list[$i] -eq $b_compare_list[$i]) {
-        $count++
-    }
+
+for ($i = 0; $i -lt 5000000; $i++)
+{
+	if (($a_compare_list[$i] -band $bitmask) -eq ($b_compare_list[$i] -band $bitmask))
+ { 
+		$count++
+	}
 }
 
-#should be 285
+#285
 $count
