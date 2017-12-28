183b24e3a09822f43b4274b0566e1c017e2d8bdd
bowmorek <kenneth.b.macneal@gmail.com>
2017-12-28 10:02:52 -0600

    update

--- 15/solution1.ps1
+++ 15/solution1.ps1
@@ -1,42 +1,25 @@
-$factor_a = 16807
-$factor_b = 48271
+[long]$factor_a = 16807
+[long]$factor_b = 48271
 
-$gen_a_start = 722
-$gen_b_start = 354
+[long]$gen_a_start = 722
+[long]$gen_b_start = 354
 
 #use modulo
-$shared_divisor = 2147483647
+[long]$shared_divisor = 2147483647
 
-function calc_value([long]$value, $multiple, $divisor) {
-    [long]$val = ($value * $multiple) % $divisor
-
-    return $val
-}
-
-$count = 0
-
-$a_val_list = new-object 'System.Collections.Generic.List[long]'
-
-$b_val_list = new-object 'System.Collections.Generic.List[long]'
-
-[void]$a_val_list.Add($gen_a_start)
-
-[void]$b_val_list.Add($gen_b_start)
+[long]$a_val = $gen_a_start
+[long]$b_val = $gen_b_start
 
 $bitmask = 65535
 
+$count = 0
 
 for ($i = 0; $i -lt 40000000; $i++) {
-	
-    [long]$a_val = calc_value -value $($a_val_list[$a_val_list.Count - 1]) -multiple $factor_a -divisor $shared_divisor
-    [void]$a_val_list.Add($a_val)
-    $a_compare = $($a_val -band $bitmask)
-	
-    [long]$b_val = calc_value -value $($b_val_list[$b_val_list.Count - 1]) -multiple $factor_b -divisor $shared_divisor
-    [void]$b_val_list.Add($b_val)
-    $b_compare = $($b_val -band $bitmask)
-
-    if($a_compare -eq $b_compare){$count++}
+    $prev_a = $a_val
+    $prev_b = $b_val
+    [long]$a_val = ($prev_a * $factor_a) % $shared_divisor
+    [long]$b_val = ($prev_b * $factor_b) % $shared_divisor
+    if($($a_val -band $bitmask) -eq $($b_val -band $bitmask)){$count++}
 }
 
 #612
